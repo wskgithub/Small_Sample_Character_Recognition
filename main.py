6ebb5ae8e51d63e2fba08411d8da8fbeb1f4b3c7
shaokun wang <mail@wangsk.xyz>
2018-09-17 14:55:05 +0800

    删除 移除繁多复杂的命令参数，简化命令操作
    将所有训练模式选择整合在“-f”参数下，1为训练参考数据集，2训练比赛数据集顶层类型，3训练比赛数据集小类。
    数据集路径参数统一使用“-tr”表示训练数据集，“-ts”表示测试数据集
    图片分类模式整合至“-c”参数下，1为对测试集所有数据进行分类，2为对一张图片进行分类

--- main.py
+++ main.py
@@ -21,44 +21,31 @@ KIND_LISTS = ['Angelic','Atemayar_Qelisayer', 'Atlantean', 'Aurek-Besh', 'Avesta
               'Oriya', 'Sylheti', 'Syriac_(Serto)', 'Tengwar', 'Tibetan', 'ULOG']
 
 def args_parse():
-    # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
     # 功能选择
-    ap.add_argument("-f1", "--function1", default=False, type=bool,
-                    help="function1: train background model")
-    ap.add_argument("-f2", "--function2", default=False, type=bool,
-                    help="function2: train evaluation top model")
-    ap.add_argument("-f3", "--function3", default=False, type=bool,
-                    help="function3: train evaluation model")
+    """
+    -f
+    1:train background model
+    2:train evaluation top model
+    3:train evaluation model
+    """
+    ap.add_argument("-f", "--function", default=False, type=int,
+                    help="Function Selection")
 
+    """
+    -c
+    1:To classify pictures in batches
+    2:Classify a single picture
+    """
     # 图片分类模式选择
-    ap.add_argument("-c1", "--classify1", default=False, type=bool,
-                    help="classify1: To classify pictures in batches")
-    ap.add_argument("-c2", "--classify2", default=False, type=bool,
-                    help="classify1: Classify a single picture")
+    ap.add_argument("-c", "--classify", default=False, type=int,
+                    help="Test classify")
 
-    # 参考数据集路径
-    ap.add_argument("-btrp", "--background_train_path", default=None, type=str,
-                    help="path to train background image")
-    ap.add_argument("-btep", "--background_test_path", default=None, type=str,
-                    help="path to test background image")
-    # 比赛数据集上层类型路径
-    ap.add_argument("-ettrp", "--evaluation_top_train_path", default=None, type=str,
-                    help="path to train evaluation top image")
-    ap.add_argument("-ettep", "--evaluation_top_test_path", default=None, type=str,
-                    help="path to test evaluation top image")
-    # 比赛数据集路径
-    ap.add_argument("-etrp", "--evaluation_train_path", default=None, type=str,
-                    help="path to train evaluation image")
-    ap.add_argument("-etep", "--evaluation_test_path", default=None, type=str,
-                    help="path to test evaluation image")
-
-    # 需分类图片路径
-    ap.add_argument("-cip", "--classify_image_path", default=None, type=str,
-                    help="The path that you want to categorize pictures in batches")
-    ap.add_argument("-coip", "--classify_one_image_path", default=None, type=str,
-                    help="The path that you want to categorize picture")
-    # args = vars(ap.parse_args())
+    # 数据集路径
+    ap.add_argument("-tr", "--train_path", default=None, type=str,
+                    help="path to train images")
+    ap.add_argument("-ts", "--test_path", default=None, type=str,
+                    help="path to test images")
     return ap.parse_args()
 
 
@@ -68,14 +55,12 @@ if __name__=='__main__':
     print('[INFO] start...')
     args = args_parse()
 
-    if args.function1 == True:
+    if args.function == 1:
         # 利用参考数据集训练模型
         # 扩展参考数据集路径
-        # background_train_path = "/home/wsk/images_bg/images_bg_train_new"
-        background_train_path = args.background_train_path
+        background_train_path = args.train_path
         # 参考测试数据集路径
-        # background_test_path = "/home/wsk/images_bg/images_bg_test_new"
-        background_test_path = args.background_test_path
+        background_test_path = args.test_path
         # 读取参考训练数据和测试数据
         print("[INFO] loading background images...")
         x_train_background, y_train_background = load_images(background_train_path, IMAGE_SIZE)
@@ -88,15 +73,12 @@ if __name__=='__main__':
         print("[INFO] compiling background model...")
         train(background_model, x_train_background, y_train_background, x_test_background, y_test_background,
               len(y_train_background) // 10, EPOCHS, 'models/backgroud_model.h5')
-
-    if args.function2 == True:
+    elif args.function == 2:
         # 使用迁移学习训练比赛集
         # 比赛训练数据集顶层类型(大类)
-        # evaluation_top_train_path "
-        evaluation_top_train_path = args.evaluation_top_train_path
+        evaluation_top_train_path = args.train_path
         # 比赛测试数据集
-        # evaluation_top_test_path = "/home/wsk/HW_Image/images_evaluation_top_kind_test"
-        evaluation_top_test_path = args.evaluation_top_test_path
+        evaluation_top_test_path = args.test_path
         # 读取比赛训练数据和测试数据
         print("[INFO] loading evaluation images...")
         x_train_top_evaluation, y_train_top_evaluation = load_images(evaluation_top_train_path, IMAGE_SIZE)
@@ -110,21 +92,19 @@ if __name__=='__main__':
               x_test_top_evaluation, y_test_top_evaluation, len(y_train_top_evaluation) // 10,
               EPOCHS, 'models/evaluation_top_model.h5')
 
-    if args.function3 == True:
+    elif args.function == 3:
         # 使用大类训练好的模型对每个大类中的小类进行分类器的训练
         # 比赛数据训练集路径(按小类划分)
-        # evaluation_train_path = '/home/wsk/HW_Image/images_evaluation_train'
-        evaluation_train_path = args.evaluation_train_path
+        evaluation_train_path = args.train_path
         # 比赛数据测试集路径(按小类划分)
-        # evaluation_test_path = '/home/wsk/HW_Image/images_evaluation_test'
-        evaluation_test_path = args.evaluation_test_path
+        evaluation_test_path = args.test_path
         # 获取训练集和测试集文件列表
         train_kind_lists = get_file_list(evaluation_train_path)
         test_kind_lists = get_file_list(evaluation_test_path)
         # 训练模型
         train_subclass(train_kind_lists, test_kind_lists, IMAGE_SIZE, EPOCHS)
 
-    if args.classify1 == True:
+    if args.classify == 1:
         # 对图片进行批量分类
         print('[INFO] start...')
         time_start = time.time()
@@ -133,15 +113,13 @@ if __name__=='__main__':
         # 读取用于顶层分类的模型
         top_model = load_model(top_model_path)
         # 需分类的图片路径
-        # classify_image_path = '/home/wsk/HW_Image/images_evaluation_test'
-        classify_image_path = args.classify_image_path
+        classify_image_path = args.test_path
         image_lists = load_test_image(classify_image_path, IMAGE_SIZE)
         # 开始送入模型分类
         classify_image(top_model, image_lists, KIND_LISTS)
         time_end = time.time()
         print('运行用时:{}'.format(time_end - time_start))
-
-    if args.classify2 == True:
+    elif args.classify == 2:
         # 对单张图片进行分类
-        classify_one_image_path = args.classify_one_image_path
+        classify_one_image_path = args.test_path
         classify_one_image(classify_one_image_path, IMAGE_SIZE, KIND_LISTS)
